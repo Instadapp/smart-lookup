cc4a3e9a17cdafa1c4722ccdcccd7ca7d10048a2
Georges KABBOUCHI <georges.kabbouchi@gmail.com>
2022-06-09 18:41:52 +0300

    Update app.vue

--- app.vue
+++ app.vue
@@ -2,7 +2,9 @@
   <Html class="h-full bg-gray-50">
     <Body class="h-full" />
   </Html>
-  <div class="min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+  <div class="min-h-full flex flex-col items-center justify-center py-12 sm:px-6 lg:px-8">
     <NuxtPage />
+
+    <div class="mt-3">Made with ❤️ by <a href="https://instadapp.io" class="hover:underline font-medium" target="_blank" rel="noopener noreferrer">Instadapp</a></div>
   </div>
 </template>
